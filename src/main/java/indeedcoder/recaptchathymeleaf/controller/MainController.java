0e808343a69d2b45373ca68760774c93327609ad
Aman Kumar <amankumar.m99@gmail.com>
2025-07-05 01:07:59 +0530

    added recaptcha response data to the UI

--- src/main/java/indeedcoder/recaptchathymeleaf/controller/MainController.java
+++ src/main/java/indeedcoder/recaptchathymeleaf/controller/MainController.java
@@ -1,6 +1,5 @@
 package indeedcoder.recaptchathymeleaf.controller;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,16 +22,18 @@ public class MainController {
 	}
 
 	@PostMapping("/save")
-	public String saveData(@ModelAttribute SampleModel sm, @RequestParam("g-recaptcha-response") String reCaptchaResponse, Model m) {
+	public String saveData(@ModelAttribute SampleModel sm,
+			@RequestParam("g-recaptcha-response") String reCaptchaResponse, Model m) {
 		String url = "https://www.google.com/recaptcha/api/siteverify";
 		String key = "6LcVSXUrAAAAANahPTSo6ca37lpOAhiD1cqWkdlh";
-		String q1 = "secret="+key;
-		String q2 = "response="+reCaptchaResponse;
+		String query = "secret=" + key + "&" + "response=" + reCaptchaResponse;
+		String finalUrl = url + "?" + query;
 		RestTemplate rt = new RestTemplate();
-		ResponseEntity<ReCaptchaResponse> resonse = rt.postForEntity(url+"?"+q1+"&"+q2, null, ReCaptchaResponse.class);
-		String s = (resonse.getBody().isSuccess())?"PASS":"FAIL";
+		ReCaptchaResponse response = rt.postForEntity(finalUrl, null, ReCaptchaResponse.class).getBody();
+		String s = (response.isSuccess()) ? "PASS" : "FAIL";
 		m.addAttribute("data", sm);
 		m.addAttribute("captchaStatus", s);
+		m.addAttribute("captchaData", response);
 		return "ResultPage";
 	}
 
